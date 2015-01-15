9222b3f22ddbc166bc4d4b67e1df496ea23f93f6
kj <kouji.wtnb@gmail.com>
2015-01-15 23:35:29 +0900

    commit

--- src/main/java/FileVisit.java
+++ src/main/java/FileVisit.java
@@ -5,17 +5,45 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.FileVisitResult;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FileVisit {
 	public static void main(String[] args) throws IOException {
+
 		Path path = Paths.get("./src").toAbsolutePath();
-		Files.walkFileTree(path, new SimpleFileVisitor<Path>(){
-			@Override
-			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-				System.out.println(file.toString());
-				return FileVisitResult.CONTINUE;
+		{
+			SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>(){
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+					System.out.println(file.toString());
+					return FileVisitResult.CONTINUE;
+				}
+			};
+			Files.walkFileTree(path, visitor);
+		}
+
+		{
+			Visitor visitor = new Visitor();
+			Files.walkFileTree(path, visitor);
+			List<String> lists = visitor.getResult();
+			for(String absPath : lists){
+				System.out.println(absPath);
 			}
-		});
+		}
+
+	}
+	public static class Visitor extends SimpleFileVisitor<Path>{
+		List<String> fileLists = new ArrayList<String>();
+
+		@Override
+		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+			fileLists.add(file.toString());
+			return FileVisitResult.CONTINUE;
+		}
+		public List<String> getResult(){
+			return fileLists;
+		}
 	}
 }
