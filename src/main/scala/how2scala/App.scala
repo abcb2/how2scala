21b7ab0f5e799d99b71ac7c31772e81c84021174
kj <kouji.wtnb@gmail.com>
2015-01-19 22:28:28 +0900

    add

--- src/main/scala/how2scala/App.scala
+++ src/main/scala/how2scala/App.scala
@@ -1,7 +1,27 @@
 package how2scala
 
-object App {
-  def main(args: Array[String]) = {
-    println("hello")
+class Apple
+object Apple {
+  def unapply(a: Any):Boolean = {
+    if(a.isInstanceOf[Apple]) true else false
+  }
+}
+
+class Orange(val name:String)
+object Orange {
+  def apply(name: String):Orange = new Orange(name)
+  def unapply(a: Orange):Option[String] = Some(a.name)
+}
+
+object Hoge {
+  def main(args: Array[String]){
+    matchTest(Orange("DEKOPONN"))
+  }
+  def matchTest(value:Any) = {
+    value match {
+      case Apple => println("Apple")
+      case Orange(name) => println("ORANGE->" + name)
+      case _ => println("OTHER")
+    }
   }
 }
